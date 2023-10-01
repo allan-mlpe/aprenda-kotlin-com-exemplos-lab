02948d98f0bb4d6b455d7f251821fa77a235d481
Allan Monteiro de Lima <allan_mlpe@hotmail.com>
2023-10-01 12:27:54 -0300

    feat: adding some logic

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val nome: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel = Nivel.BASICO)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        usuarios.forEach { inscritos.add(it) }
+    }
+    
+    infix fun possuiMatriculaDe(usuario: Usuario) : Boolean {
+        return inscritos.contains(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Joca")
+    val usuario2 = Usuario("Bob")
+    val usuario3 = Usuario("João")
+    
+    val portugol = ConteudoEducacional("Lógica de Programação")
+    val java = ConteudoEducacional(nome = "Introdução a OO", nivel = Nivel.INTERMEDIARIO)
+    val kotlin = ConteudoEducacional("Programação assíncrona", 120, Nivel.AVANCADO)
+    
+    val conteudos = listOf(portugol, java, kotlin)
+    
+    val formacao = Formacao("Linguagens da JVM", conteudos)
+    
+    formacao.matricular(usuario1, usuario2)
+    
+    println(formacao)
+    
+    println(formacao possuiMatriculaDe usuario1)
+    println(formacao possuiMatriculaDe usuario2)
+    println(formacao possuiMatriculaDe usuario3)
 }
